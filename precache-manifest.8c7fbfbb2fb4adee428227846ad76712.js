be503690440c20a69f987648b73e5b0e0bbbbce4
Nathan Barry <Nathanbarry474@gmail.com>
2020-05-31 14:57:49 -0500

    Updates

---
+++ precache-manifest.8c7fbfbb2fb4adee428227846ad76712.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cfb94505b286843799e10dd952ea17c",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "f1935b4be19e8489b2dd",
+    "url": "/to-do-list/static/css/main.34825f19.chunk.css"
+  },
+  {
+    "revision": "0e569a7a06dc61168a73",
+    "url": "/to-do-list/static/js/2.892d83ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.892d83ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1935b4be19e8489b2dd",
+    "url": "/to-do-list/static/js/main.4cd4db87.chunk.js"
+  },
+  {
+    "revision": "fa27aabefbb81c21a734",
+    "url": "/to-do-list/static/js/runtime-main.80f3eba2.js"
+  }
+]);
